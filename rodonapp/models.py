b65d98af85a3c22f3efc1fbb0b76a2d7f3171167
devu112 <devikathrayambakam112@gmail.com>
2024-04-03 19:07:54 +0530

    commit 2

--- rodonapp/models.py
+++ rodonapp/models.py
@@ -6,7 +6,7 @@ class ContactMessage(models.Model):
     subject = models.CharField(max_length=200)
     message = models.TextField()
     email = models.EmailField(null='True')
-    phone = models.IntegerField(null='True')
+    phone = models.CharField(max_length=20)
 
     def __str__(self):
         return self.subject
